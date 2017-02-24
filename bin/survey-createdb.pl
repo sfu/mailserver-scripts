e2fc0271523c0bc712b44b6e344230d5898506be
Steve Hillman <hillman@sfu.ca>
2017-02-23 16:42:42 -0800

    Split input file on whitespace rather than single tab

--- bin/survey-createdb.pl
+++ bin/survey-createdb.pl
@@ -25,7 +25,7 @@ if (!$survey)
 tie %aliases,"DB_File","$SurveyDBPath/${survey}_survey.db",O_RDWR|O_CREAT,0666,$DB_HASH ;
 
 while ( <STDIN> ) {
-	my ($sfuid, $account, $mailalias, $nsseaddress) = split /\t/, $_ ;
+	my ($sfuid, $account, $mailalias, $nsseaddress) = split /\s+/, $_ ;
 	chomp ( $nsseaddress ) ;
 	$aliases{$mailalias} = $account . '@sfu.ca' ;
 }
