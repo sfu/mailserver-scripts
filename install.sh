c70a48da2ce508a74f9e53a3a3b55c67a605dc36
Steve Hillman <hillman@sfu.ca>
2019-06-20 22:04:49 -0700

    Add LWP HTTPS module to installer

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ export https_proxy=http://proxy.sfu.ca:8080
 export ftp_proxy=http://proxy.sfu.ca:8080
 
 # Install dependencies and whatever perl modules we can via Yum
-yum install -y gcc perl-CPAN perl-libwww-perl perl-XML-LibXML perl-XML-Simple perl-MailTools perl-JSON perl-Sys-Syslog perl-DB_File
+yum install -y gcc perl-CPAN perl-libwww-perl perl-XML-LibXML perl-XML-Simple perl-MailTools perl-JSON perl-Sys-Syslog perl-DB_File perl-LWP-Protocol-https
 
 # Precreate CPAN config
 #
